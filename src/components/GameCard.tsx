555a4fd80095e2fb2555d11761d6fcc9d74b0a1c
Parul Sharma <parul@hospopay.com>
2023-07-10 22:59:40 +1000

    add emoji

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import {Card, Image, CardBody, Heading, HStack} from '@chakra-ui/react';
 import { PlatformIconList } from './PlatformIconList';
 import { CriticScore } from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import Emoji from './Emoji';
 
 interface Props {
     game: Game
@@ -19,7 +20,7 @@ const GameCard = ({game}: Props) => {
     <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}></PlatformIconList>
     <CriticScore score={game.metacritic}/>
     </HStack>
-    <Heading fontSize='2xl'>{game.name}</Heading>
+    <Heading fontSize='2xl'>{game.name} <Emoji rating={game.rating_top}></Emoji></Heading>
    
     </CardBody>
 
